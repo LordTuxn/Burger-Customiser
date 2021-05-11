1be6fe9ff3ff55eb4c671a59353d16dedc09f1cd
R3tuxn <r3tuxn@gmail.com>
2021-05-11 16:17:40 +0200

    Updated App.xaml

--- Burger-Customiser/App.xaml.cs
+++ Burger-Customiser/App.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using Burger_Customiser_DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 using System.Windows;
+using System.IO;
 
 namespace Burger_Customiser {
 
@@ -9,25 +11,33 @@ namespace Burger_Customiser {
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application {
-        private readonly ServiceProvider serviceProvider;
 
+        private readonly ServiceProvider serviceProvider;
+        private IConfiguration config;
+        
         public App() {
+            ConfigureConfiguration();
+            
             ServiceCollection services = new ServiceCollection();
             ConfigureServices(services);
             serviceProvider = services.BuildServiceProvider();
         }
 
-        private void ConfigureServices(IServiceCollection services) {
-            services.AddEntityFrameworkSqlite()
-                .AddDbContext<ArticleDBContext>(options => {
-                    options.UseSqlite("Connection");
-                });
+        public void ConfigureServices(IServiceCollection service) {
+            service.AddSingleton<StartWindow>();
+            service.Configure<StartWindow>(config.GetSection("Application"));
 
-            services.AddSingleton<StartWindow>();
+            service.AddDbContext<ArticleDBContext>(options => {
+                string connectionString = config.GetSection("Database").GetSection("ConnectionString").Value;
+                options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+            });
         }
-
-        private void OnStartup(object sender, StartupEventArgs e) {
-            serviceProvider.GetService<StartWindow>().Show();
+        
+        public void ConfigureConfiguration() {
+            IConfigurationBuilder configBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+            config = configBuilder.Build();
         }
     }
 }
