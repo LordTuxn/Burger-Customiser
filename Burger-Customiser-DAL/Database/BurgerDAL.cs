19869b6374b673c176b5989a8df398b737c211c8
LordTuxn <r3tuxn@gmail.com>
2021-05-29 22:21:01 +0200

    Added two footer buttons

--- Burger-Customiser-DAL/Database/BurgerDAL.cs
+++ Burger-Customiser-DAL/Database/BurgerDAL.cs
@@ -1,8 +1,4 @@
 ﻿using Burger_Customiser_BLL;
-using Burger_Customiser_BLL.Relationships;
-using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace Burger_Customiser_DAL.Database {
     public class BurgerDAL {
@@ -20,26 +16,6 @@ namespace Burger_Customiser_DAL.Database {
 
         public void UpdateBurger(Burger burger) {
             context.Update(burger);
-            context.SaveChanges();
-        }
-
-        public List<BurgerIngredient> GetBurgerIngredients(Burger burger) {
-            //return burger.BurgerIngredients;
-            return context.BurgerIngredients.FromSqlRaw($@"SELECT * FROM burger_has_ingredients WHERE B_ID = {burger.ID}").ToList();
-        }
-
-        public void AddBurgerIngredient(BurgerIngredient burgerIngredient) {
-            context.Add(burgerIngredient);
-            context.SaveChanges();
-        }
-
-        public void UpdateBurgerIngredient(BurgerIngredient burgerIngredient) {
-            context.Update(burgerIngredient);
-            context.SaveChanges();
-        }
-
-        public void RemoveBurgerIngredient(BurgerIngredient burgerIngredient) {
-            context.Remove(burgerIngredient);
             context.SaveChanges();
         }
     }
--- Burger-Customiser/UserControls/NavigationFooter.xaml.cs
+++ Burger-Customiser/UserControls/NavigationFooter.xaml.cs
@@ -22,5 +22,13 @@ namespace Burger_Customiser.UserControls
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e) {
+
+        }
     }
 }
