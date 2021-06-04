5853c9d800d16b4d0df2d15e11d1b34086160eb0
LordTuxn <r3tuxn@gmail.com>
2021-06-04 02:00:20 +0200

    Added Article Option Page

---
+++ Burger-Customiser/Navigation/Pages/ArticleOption/ArticleOptionPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Burger_Customiser.Navigation.Pages.ArticleOption {
+    /// <summary>
+    /// Interaction logic for ArticleOptionPage.xaml
+    /// </summary>
+    public partial class ArticleOptionPage : Page {
+        public ArticleOptionPage() {
+            InitializeComponent();
+        }
+
+        private void BurgerCustomiser_Click(object sender, MouseButtonEventArgs e) {
+            // TODO: Set order option and send to next page
+        }
+
+        private void ProductCatalogue_Click(object sender, MouseButtonEventArgs e) {
+            // TODO: Set order option and send to next page
+        }
+    }
+}
---
+++ Burger-Customiser/Navigation/Pages/ArticleOption/ArticleOptionPageVM.cs
@@ -0,0 +1,30 @@
+﻿using Burger_Customiser.Navigation.Messages;
+using Burger_Customiser.Navigation.Pages.OrderOption;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Messaging;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace Burger_Customiser.Navigation.Pages.ArticleOption {
+    public class ArticleOptionPageVM : ViewModelBase, IPageViewModel {
+
+        [Obsolete("Only for design data!", true)]
+        public ArticleOptionPageVM() {
+            if (!IsInDesignMode) {
+                throw new Exception("Use only for design mode");
+            }
+        }
+
+        public ArticleOptionPageVM(ILogger<ArticleOptionPageVM> logger) {
+            logger.LogInformation($"Successfully Registered: {nameof(ArticleOptionPageVM)}");
+        }
+
+        public void BackPage() {
+            Messenger.Default.Send(new ChangePageMessage(typeof(OrderOptionPageVM)));
+        }
+
+        public void ContinuePage() {
+            throw new NotImplementedException();
+        }
+    }
+}
