803476c6e3a387e840a0c69391ae7142c330e72b
LordTuxn <r3tuxn@gmail.com>
2021-06-16 18:27:02 +0200

    Fixed article items using full height

---
+++ Burger-Customiser/Utils/ArticleListRowsConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Burger_Customiser.Utils {
+    public class ArticleListRowsConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            // Set min size of rows to prevent a higher height for article items
+            return (int)value <= 3 ? 3 : value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
